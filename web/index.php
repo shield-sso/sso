a0fce5bf80d3f8ab83d8b5db639d46502f0ad27b
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2016-12-24 13:43:58 +0100

    silex init

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use ShieldSSO\Application;
+use Silex\Provider\TwigServiceProvider;
+
+$app = new Application();
+$app['debug'] = true;
+
+$app->register(new TwigServiceProvider, ['twig.path' => __DIR__ . '/../resources/views']);
+
+$app->get('/', function () use ($app) {
+    return $app->render('index.html.twig');
+});
+
+$app->run();
