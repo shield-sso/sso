3be7aaf55816d802ded31c387f6fa83d4a942dd9
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2016-12-24 14:06:22 +0100

    test route

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use ShieldSSO\Application;
 use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 $app = new Application();
 $app['debug'] = true;
@@ -12,6 +13,10 @@ $app->register(new TwigServiceProvider, ['twig.path' => __DIR__ . '/../resources
 
 $app->get('/', function () use ($app) {
     return $app->render('index.html.twig');
+});
+
+$app->get('/authorize', function () {
+    return new JsonResponse(['authorization_code' => 'code']);
 });
 
 $app->run();
