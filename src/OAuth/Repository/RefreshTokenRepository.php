6e2887a84d33fc93fed247794c119853e8c62648
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2016-12-26 14:44:18 +0100

    refresh token implementation second part

--- src/OAuth/Repository/RefreshTokenRepository.php
+++ src/OAuth/Repository/RefreshTokenRepository.php
@@ -7,9 +7,21 @@ namespace ShieldSSO\OAuth\Repository;
 use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
 use League\OAuth2\Server\Repositories\RefreshTokenRepositoryInterface;
 use ShieldSSO\OAuth\Entity\RefreshToken;
+use ShieldSSO\Contract\Repository\RefreshTokenRepositoryInterface as AppRepositoryInterface;
 
 class RefreshTokenRepository implements RefreshTokenRepositoryInterface
 {
+    /** @var AppRepositoryInterface */
+    private $appRepository;
+
+    /**
+     * @param AppRepositoryInterface $appRepository
+     */
+    public function __construct(AppRepositoryInterface $appRepository)
+    {
+        $this->appRepository = $appRepository;
+    }
+
     /**
      * @inheritdoc
      */
@@ -28,14 +40,22 @@ class RefreshTokenRepository implements RefreshTokenRepositoryInterface
     /**
      * @inheritdoc
      */
-    public function revokeRefreshToken($tokenId): void
+    public function revokeRefreshToken($code): void
     {
+        $refreshToken = $this->appRepository->getByCode($code);
+        $refreshToken->setRevoked(true);
+
+        $this->appRepository->persist($refreshToken);
+        $this->appRepository->flush();
     }
 
     /**
      * @inheritdoc
      */
-    public function isRefreshTokenRevoked($tokenId): bool
+    public function isRefreshTokenRevoked($code): bool
     {
+        $refreshToken = $this->appRepository->getByCode($code);
+
+        return $refreshToken->isRevoked();
     }
 }
