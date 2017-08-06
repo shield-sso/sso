935f9bfe19225bd7adae6c9dffd34775c9917f1b
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2017-08-06 11:53:07 +0200

    provision dev machine

---
+++ puphpet/files/exec-always/provision.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd /var/www/
+
+composer install
+
+cd /var/www/resources/keys
+
+if ! [ -e private.key ]
+then
+    openssl genrsa -out private.key 1024
+fi
+
+if ! [ -e public.key ]
+then
+    openssl rsa -in private.key -pubout -out public.key
+fi
+
+cd /var/www/resources/config
+
+if ! [ -e parameters.yml ]
+then
+    cp parameters.dist parameters.yml
+fi
